16403a28d02d80ec08a957914840191d56c02697
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-05-14 01:14:43 +0800

    Revert "frame changes"
    This reverts commit c0f7bae12c55d17d8efa646776af78a9259bc811.

--- Sources/BarChart/BarChartView.swift
+++ Sources/BarChart/BarChartView.swift
@@ -46,33 +46,30 @@ public struct SelectableBarChartView<SelectionView: View, Fill: ShapeStyle> : Vi
     }
     
     public var body: some View {
-        GeometryReader { rootProxy in
+        GeometryReader { proxy in
             HStack {
                 ScrollView(.horizontal) {
-                    GeometryReader { proxy in
-                        ZStack {
-                            CoordinateSystemView(yAxis: self.yAxis,
-                                                 xAxis: self.xAxis,
-                                                 frameSize: proxy.size).id(UUID())
-                                .onReceive(self.config.objectWillChange) { _ in
-                                    self.yAxis = YAxis(frameHeight: self.yAxisHeight(proxy.size.height),
-                                                       data: self.config.data.yValues,
-                                                       ref: self.config.yAxis,
-                                                       labelsCTFont: self.config.labelsCTFont)
-                                    self.xAxis = XAxis(frameWidth: proxy.size.width - self.yAxis.maxLabelWidth,
-                                                       data: self.config.data.entries,
-                                                       ref: self.config.xAxis,
-                                                       labelsCTFont: self.config.labelsCTFont)
-                                }
-                            self.selectionView
-                            BarChartCollectionView(yAxis: self.yAxis,
-                                                   xAxis: self.xAxis,
-                                                   selectionCallback: self.$selectionCallback)
-                        }
-                        
+                    ZStack {
+                        CoordinateSystemView(yAxis: self.yAxis,
+                                             xAxis: self.xAxis,
+                                             frameSize: proxy.size).id(UUID())
+                            .onReceive(self.config.objectWillChange) { _ in
+                                self.yAxis = YAxis(frameHeight: self.yAxisHeight(proxy.size.height),
+                                                   data: self.config.data.yValues,
+                                                   ref: self.config.yAxis,
+                                                   labelsCTFont: self.config.labelsCTFont)
+                                self.xAxis = XAxis(frameWidth: proxy.size.width - self.yAxis.maxLabelWidth,
+                                                   data: self.config.data.entries,
+                                                   ref: self.config.xAxis,
+                                                   labelsCTFont: self.config.labelsCTFont)
+                            }
+                        self.selectionView
+                        BarChartCollectionView(yAxis: self.yAxis,
+                                               xAxis: self.xAxis,
+                                               selectionCallback: self.$selectionCallback)
                     }
-                    YAxisLabelView(yAxis: yAxis, height: rootProxy.size.height)
                 }
+                //YAxisLabelView(yAxis: yAxis, frameSize: proxy.size)
             }
         }
     }
--- Sources/BarChart/CoordinateSystemView.swift
+++ Sources/BarChart/CoordinateSystemView.swift
@@ -163,7 +163,7 @@ struct YAxisView: View {
 
 struct YAxisLabelView: View {
     let yAxis: YAxis
-    let height: CGFloat
+    let frameSize: CGSize
     
     var body: some View {
         ForEach((0..<self.yAxis.formattedLabels().count), id: \.self) { index in
@@ -178,7 +178,7 @@ struct YAxisLabelView: View {
     
     func labelOffsetY(at index: Int) -> CGFloat {
         let tickY = self.tickY(at: index)
-        let height = self.height
+        let height = self.frameSize.height
         let y = (height - tickY) - (height / 2)
         return y
     }
