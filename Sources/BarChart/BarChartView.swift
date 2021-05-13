ed534277218b83403d3a5390cf0c0e8e1f9c0aee
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-05-14 00:37:12 +0800

    change onreceive location to (attempt) prevent recursion

--- Sources/BarChart/BarChartView.swift
+++ Sources/BarChart/BarChartView.swift
@@ -53,22 +53,22 @@ public struct SelectableBarChartView<SelectionView: View, Fill: ShapeStyle> : Vi
                         CoordinateSystemView(yAxis: self.yAxis,
                                              xAxis: self.xAxis,
                                              frameSize: proxy.size).id(UUID())
-                            .onReceive(self.config.objectWillChange) { _ in
-                                self.yAxis = YAxis(frameHeight: self.yAxisHeight(proxy.size.height),
-                                                   data: self.config.data.yValues,
-                                                   ref: self.config.yAxis,
-                                                   labelsCTFont: self.config.labelsCTFont)
-                                self.xAxis = XAxis(frameWidth: proxy.size.width - self.yAxis.maxLabelWidth,
-                                                   data: self.config.data.entries,
-                                                   ref: self.config.xAxis,
-                                                   labelsCTFont: self.config.labelsCTFont)
-                            }
                         self.selectionView
                         BarChartCollectionView(yAxis: self.yAxis,
                                                xAxis: self.xAxis,
                                                selectionCallback: self.$selectionCallback)
                     }
                     XAxisLabelView(xAxis: self.xAxis, frameSize: proxy.size)
+                }
+                .onReceive(self.config.objectWillChange) { _ in
+                    self.yAxis = YAxis(frameHeight: self.yAxisHeight(proxy.size.height),
+                                       data: self.config.data.yValues,
+                                       ref: self.config.yAxis,
+                                       labelsCTFont: self.config.labelsCTFont)
+                    self.xAxis = XAxis(frameWidth: proxy.size.width - self.yAxis.maxLabelWidth,
+                                       data: self.config.data.entries,
+                                       ref: self.config.xAxis,
+                                       labelsCTFont: self.config.labelsCTFont)
                 }
             }
         }
